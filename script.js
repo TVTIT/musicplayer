cc22bdfca73e64af5ff37d3e61407cec94fe7713
TVTIT <45415602+TVTIT@users.noreply.github.com>
2020-02-07 15:27:56 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -21,10 +21,70 @@ $(function()
 	var playPreviousTrackButton = $('#play-previous'), playNextTrackButton = $('#play-next'), currIndex = -1;
 	
 	var songs = [{
-		artist: "Dig Didzay",
-		name: "Nếu Anh Đi (Cover)",
-		url: "Musics/NeuAnhDi.mp3",
-		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+		artist: "Quân A.P",
+		name: "Ai Là Người Thương Em",
+		url: "Musics/Ai Là Người Thương Em - Quân A.P.mp3",
+		picture: "https://photo-zmp3.zadn.vn/cover/b/1/2/d/b12d716b2c97575bbb278c2a49d45d61.jpg"
+	},
+	{
+		artist: "Jack, K-ICM",
+		name: "Bạc Phận",
+		url: "Musics/Bạc Phận - Jack ft. K-ICM.flac",
+		picture: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/4/2/5/3/425334e6f252b8c34d74d16177a5eb9d.jpg"
+	},
+	{
+		artist: "Lê Bảo Bình",
+		name: "Bước Qua Đời Nhau",
+		url: "Musics/Bước Qua Đời Nhau - Lê Bảo Bình.flac",
+		picture: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/0/c/1/7/0c17295b34844a5ad26ae2145d62b4fc.jpg"
+	},
+	{
+		artist: "Jack, K-ICM",
+		name: "Em Gì Ơi",
+		url: "Musics/Em Gì Ơi - Jack x K-ICM.flac",
+		picture: "https://static-zmp3.zadn.vn/skins/zma-2019/images/decuv2/emgioi.jpg"
+	},
+	{
+		artist: "Jack (G5R)",
+		name: "Hồng Nhan",
+		url: "Musics/Hồng Nhan - Jack (G5R).flac",
+		picture: "https://zmp3-photo-fbcrawler.zadn.vn/cover/3/2/7/f/327f68099674128289ba8a2e98232d68.jpg"
+	},
+	{
+		artist: "Lê Bảo Bình",
+		name: "Lá Xa Lìa Cành",
+		url: "Musics/Lá Xa Lìa Cành - Lê Bảo Bình.flac",
+		picture: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/d/2/a/8/d2a8fe2f80b1da9ed5ca4d3fc921446f.jpg"
+	},
+	{
+		artist: "Đình Dũng",
+		name: "Sai Lầm Của Anh",
+		url: "Musics/Sai Lầm Của Anh - Đình Dũng.flac",
+		picture: "https://avatar-nct.nixcdn.com/song/2019/10/21/4/6/1/e/1571623691373_640.jpg"
+	},
+	{
+		artist: "Jack, K-ICM",
+		name: "Sóng Gió",
+		url: "Musics/Sóng Gió Jack x K-ICM.flac",
+		picture: "https://static-zmp3.zadn.vn/skins/zma-2019/images/winner/songgio.jpg"
+	},
+	{
+		artist: "Phan Duy Anh",
+		name: "Từng Yêu",
+		url: "Musics/Từng Yêu - Phan Duy Anh.flac",
+		picture: "https://lh3.googleusercontent.com/proxy/zoSH09zZ_oAoJWRcLNUU7BmV7WmFwZNP9BW6UpzVYwarw20WrqLVf7ivpPMM3EaUVCwcO1LokhOdlO2kErKZS0BuN8Oh84_AXTB30UVY0jh1HX9KR5veSXlJH4_QgV05stQ"
+	},
+	{
+		artist: "Nhật Phong, Đình Dũng",
+		name: "Tường Quân Remix",
+		url: "Musics/Tướng Quân Remix - Nhật Phong x DinhLong.flac",
+		picture: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/b/8/8/a/b88a59c8e5116807f13cd49214dcf2f8.jpg"
+	},
+	{
+		artist: "Jack, K-ICM",
+		name: "Việt Nam Tôi",
+		url: "Musics/Việt Nam Tôi - Jack x K-ICM.flac",
+		picture: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/8/b/f/9/8bf90008a57a96e1f376e76a32c26f0c.jpg"
 	}];
 	
 	function shuffle(a) {
@@ -49,6 +109,7 @@ $(function()
                 albumArt.addClass('active');
                 checkBuffering();
                 i.attr('class','fas fa-pause');
+				albumArt.find('img').attr('src', currArtwork);
                 audio.play();
             }
             else
@@ -209,6 +270,7 @@ $(function()
 			currAlbum = songs[currIndex].name;
             currTrackName = songs[currIndex].artist;
             currArtwork = songs[currIndex].picture;
+			// albumArt.find('img').attr('src', currArtwork);
 
             audio.src = songs[currIndex].url;
             
@@ -236,6 +298,7 @@ $(function()
                 --currIndex;
             else
                 ++currIndex;
+			
         }
     }
 
